a36520330c303c53f5ba6637b05c158a48695953
json <7091322262q>
2020-04-17 16:17:50 +0800

    4.17备份

--- src/app/shared/directive/input-keyup-stop.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InputKeyupStopDirective } from './input-keyup-stop.directive';
-
-describe('InputKeyupStopDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InputKeyupStopDirective();
-    expect(directive).toBeTruthy();
-  });
-});
